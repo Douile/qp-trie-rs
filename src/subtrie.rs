f8d418aa82c502ee2f14961f480531344c9fab12
Douile <25043847+Douile@users.noreply.github.com>
2023-04-09 17:56:02 +0100

    subtrie: Add str convienience functions

--- src/subtrie.rs
+++ src/subtrie.rs
@@ -4,6 +4,7 @@ use core::ops::Index;
 
 use iter::Iter;
 use node::Node;
+use wrapper::{BStr, BString};
 
 pub struct SubTrie<'a, K: 'a, V: 'a> {
     pub(crate) root: Option<&'a Node<K, V>>,
@@ -49,16 +50,40 @@ impl<'a, K: Borrow<[u8]>, V> SubTrie<'a, K, V> {
         }
     }
 
-    pub fn subtrie<L: Borrow<[u8]>>(&self, prefix: L) -> SubTrie<'a, K, V> {
+    pub fn subtrie<L: ?Sized>(&self, prefix: &L) -> SubTrie<'a, K, V>
+    where
+        L: Borrow<[u8]>,
+    {
         SubTrie {
             root: self.root.and_then(|node| node.get_prefix(prefix.borrow())),
         }
     }
 
-    pub fn get<L: Borrow<[u8]>>(&self, key: L) -> Option<&'a V> {
+    pub fn get<L: ?Sized>(&self, key: &L) -> Option<&'a V>
+    where
+        L: Borrow<[u8]>,
+    {
         self.root
             .and_then(|node| node.get(key.borrow()))
             .map(|leaf| &leaf.val)
+    }
+}
+
+impl<'b, V> SubTrie<'b, BString, V> {
+    /// Convenience function for getting with a string.
+    pub fn get_str<'a, Q: ?Sized>(&'a self, key: &Q) -> Option<&'a V>
+    where
+        Q: Borrow<str>,
+    {
+        self.get(AsRef::<BStr>::as_ref(key.borrow()))
+    }
+
+    /// Convenience function for viewing subtries wit a string prefix.
+    pub fn subtrie_str<'a, Q: ?Sized>(&'a self, prefix: &Q) -> SubTrie<'a, BString, V>
+    where
+        Q: Borrow<str>,
+    {
+        self.subtrie(AsRef::<BStr>::as_ref(prefix.borrow()))
     }
 }
 
@@ -66,6 +91,6 @@ impl<'a, K: Borrow<[u8]>, V, L: Borrow<[u8]>> Index<L> for SubTrie<'a, K, V> {
     type Output = V;
 
     fn index(&self, key: L) -> &V {
-        self.get(key).unwrap()
+        self.get(&key).unwrap()
     }
 }
